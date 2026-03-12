e5fe89e2f1a85d9158e45cecd87f59bf26a02136
Alexis Morel <22501205@me.uvsq.fr>
2026-03-12 11:47:32 +0100

    ajout de tous les autres tests

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -2,8 +2,17 @@ import unittest
 from resa.resa import *
 
 class ResaUnitTests(unittest.TestCase):
-  def test_petite(self):
-    self.assertEqual(bookMeetingRoom(3), "Small")
+    def test_petite(self):
+        self.assertEqual(bookMeetingRoom(3), "Small")
+    def test_moyenne(self):
+        self.assertEqual(bookMeetingRoom(20), "Medium")
+    def test_grande(self):
+        self.assertEqual(bookMeetingRoom(43), "Large")
+    def test_trop_grand(self):
+        self.assertEqual(bookMeetingRoom(52), "Refused")
+    def test_erreur(self):
+        self.assertEqual(bookMeetingRoom(-2), "Small")
+
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
