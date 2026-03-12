ad091dfd1640dee3acc885813b211765f424fac6
Alexis Morel <22501205@me.uvsq.fr>
2026-03-12 11:44:04 +0100

    ajout du test pour une petite salle

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -2,8 +2,8 @@ import unittest
 from resa.resa import *
 
 class ResaUnitTests(unittest.TestCase):
-  def test_bookMeetingRoom(self):
-    self.assertEqual(True, False)
+  def test_petite(self):
+    self.assertEqual(bookMeetingRoom(3), "Small")
 
 if __name__ == '__main__':
   unittest.main()
